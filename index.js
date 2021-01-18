e94bf1079bd237f2de9f16cd90f7b13de9e2f160
Jasikainen <jani.aasikainen@gmail.com>
2021-01-18 17:56:49 +0200

    exercises 1 and 2 of part 3

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express")
+const app = express()
+app.use(express.json()) 
+
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada LoveLace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendick",
+    number: "39-23-6423122",
+  }
+]
+
+app.get("/", (request, response) => {
+  // Respond with a following header
+  response.send("<h1>Part 3 of Full Stack Open</h1>")
+})
+
+app.get('/api/persons', (request, response) => {
+  // Respond with a following jsons to browser
+  console.log(request)
+  response.json(persons)
+})
+app.get('/info', (request, response) => {
+  const date = new Date()
+  response
+    .send(`Phonebook has info for ${persons.length} people.</br>${date}`)
+})
+
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}.`) 
+})
