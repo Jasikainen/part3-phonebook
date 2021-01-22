b4c85567cf54d979e34e7c2aafe2001ee84b48c8
Jasikainen <jani.aasikainen@gmail.com>
2021-01-22 11:25:17 +0200

    exercise 3.12. initialization of mongo

---
+++ mongo.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose")
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+const password = process.argv[2]
+const url = 
+`mongodb+srv://jani:${password}@cluster0.a96ed.mongodb.net/phonebook-app?retryWrites=true`
+
+mongoose.connect(url, 
+  { useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true 
+  })
+
+// Define the schema
+const personSchema = new mongoose.Schema({
+  number: Number,
+  name: String
+})
+// Create a document "template" out of schema
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length === 5) {
+  // New object of Person
+  const person = new Person({
+    number: process.argv[4],
+    name: process.argv[3]
+  })
+  // Save to database
+  person.save().then(response => {
+    console.log(`added ${response.name} number ${response.number} to phonebook`)
+    mongoose.connection.close()
+  })
+}
+
+else if (process.argv.length === 3) {
+  Person.find({}).then(result => {
+    console.log(`phonebook contains ${result.length} persons:`)
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`)
+    })
+    mongoose.connection.close()
+  })
+}
